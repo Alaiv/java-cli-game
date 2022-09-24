dd6ef598ff27dce99aec6a2250308352eddda29f
Kudriavtsev Aleksandr Sergeevich <lasacc4@yandex.ru>
2022-09-24 12:02:59 +0300

    Added utils.scanner + make

---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static String userName () {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String name = scanner.nextLine();
+        return name;
+    }
+}
